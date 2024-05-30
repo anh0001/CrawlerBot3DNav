5125c0c01011ee74f0deba3b5da9cfe2936d5651
anh0001 <anhrisn@gmail.com>
2024-05-30 11:47:52 +0900

    Update rosbag to be able to split data

--- src/robot_control/src/recording_control.py
+++ src/robot_control/src/recording_control.py
@@ -5,36 +5,58 @@ from std_msgs.msg import String
 import os
 from datetime import datetime
 
-# Global variable to keep track of recording state
+# Global variables
 recording = False
 bag = None
 bag_filename = ""
+bag_counter = 0
+bag_size_limit = 1024 * 1024 * 1024  # 1 GB
 
 def control_callback(msg):
-    global recording, bag, bag_filename
+    global recording, bag, bag_filename, bag_counter
 
     if msg.data == 'start' and not recording:
-        rospy.loginfo('Starting recording...')
-        directory = os.path.join(os.path.dirname(__file__), '../../../rosbag')
-        bag_filename = os.path.join(directory, 'recording_{}.bag'.format(datetime.now().strftime("%Y%m%d_%H%M%S")))
-        bag = rosbag.Bag(bag_filename, 'w', compression=rosbag.Compression.LZ4)
-        recording = True
-        rospy.loginfo('Recording started: {}'.format(bag_filename))
+        start_new_bag()
     elif msg.data == 'stop' and recording:
-        rospy.loginfo('Stopping recording...')
-        bag.close()
-        recording = False
-        rospy.loginfo('Recording stopped: {}'.format(bag_filename))
+        stop_recording()
     elif msg.data == 'pause' and recording:
-        rospy.loginfo('Pausing recording...')
+        pause_recording()
+
+def start_new_bag():
+    global recording, bag, bag_filename, bag_counter
+    rospy.loginfo('Starting recording...')
+    directory = os.path.join(os.path.dirname(__file__), '../../../rosbag')
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+    bag_counter += 1
+    bag_filename = os.path.join(directory, 'recording_{}_part{}.bag'.format(datetime.now().strftime("%Y%m%d_%H%M%S"), bag_counter))
+    bag = rosbag.Bag(bag_filename, 'w', compression=rosbag.Compression.LZ4)
+    recording = True
+    rospy.loginfo('Recording started: {}'.format(bag_filename))
+
+def stop_recording():
+    global recording, bag, bag_filename
+    rospy.loginfo('Stopping recording...')
+    if bag:
         bag.close()
-        recording = False
-        rospy.loginfo('Recording paused: {}'.format(bag_filename))
+    recording = False
+    rospy.loginfo('Recording stopped: {}'.format(bag_filename))
+
+def pause_recording():
+    global recording, bag, bag_filename
+    rospy.loginfo('Pausing recording...')
+    if bag:
+        bag.close()
+    recording = False
+    rospy.loginfo('Recording paused: {}'.format(bag_filename))
 
 def save_data_callback(msg, topic_name):
     global recording, bag
     if recording and bag:
         bag.write(topic_name, msg)
+        if bag.size() > bag_size_limit:
+            bag.close()
+            start_new_bag()
 
 def recording_control():
     rospy.init_node('recording_control', anonymous=True)
@@ -45,9 +67,9 @@ def recording_control():
     # Subscribe to the sensor topics and set up callbacks to save data
     sensor_topics = [
         ('/camera/color/camera_info', 'sensor_msgs/CameraInfo'),
-        ('/camera/color/image_raw/compressed', 'sensor_msgs/Image'),
+        ('/camera/color/image_raw', 'sensor_msgs/Image'),
         ('/camera/depth/camera_info', 'sensor_msgs/CameraInfo'),
-        ('/camera/depth/image_rect_raw/compressed', 'sensor_msgs/Image'),
+        ('/camera/depth/image_rect_raw', 'sensor_msgs/Image'),
         ('/camera/depth/color/points', 'sensor_msgs/PointCloud2'),
         ('/lidar/points', 'sensor_msgs/PointCloud2')
     ]
